a1899712213d2856ceba78a0aca33dad1ba9d403
ysayonnar <ysayonnar@gmail.com>
2026-02-10 20:23:49 +0300

    feat: initialized repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Scrumio
+
+**Scrumio** is a lightweight backend service for managing projects and tasks based on the Scrum methodology.
+It provides support for backlogs, boards, sprints, and tickets, offering a full CRUD workflow with a clear project structure.
---
+++ src/main/java/com/example/scrumio/ScrumioApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrumio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumioApplication.class, args);
+    }
+
+}
