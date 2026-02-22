972632d22f447f6687ed904f5f86df3b906de7c6
ysayonnar <ysayonnar@gmail.com>
2026-02-22 15:53:02 +0300

    chore: json properties refactored

--- src/main/java/com/example/scrumio/web/dto/TicketResponse.java
+++ src/main/java/com/example/scrumio/web/dto/TicketResponse.java
@@ -2,6 +2,7 @@ package com.example.scrumio.web.dto;
 
 import com.example.scrumio.entity.TicketPriority;
 import com.example.scrumio.entity.TicketStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,9 +14,17 @@ public record TicketResponse(
         TicketPriority priority,
         TicketStatus status,
         int estimation,
+
+        @JsonProperty("created_at")
         LocalDateTime createdAt,
+
+        @JsonProperty("updated_at")
         LocalDateTime updatedAt,
+
+        @JsonProperty("deleted_at")
         LocalDateTime deletedAt,
+
+        @JsonProperty("spring_id")
         UUID sprintID
 ) {
 }
