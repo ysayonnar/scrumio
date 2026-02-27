b5c757bf9e54fc8f43914ae1b4c1cb9880f141fa
ysayonnar <ysayonnar@gmail.com>
2026-02-28 00:33:53 +0300

    feat(migrations): added enums and indexes

--- src/main/resources/db/migrations/V1__create_baseline.sql
+++ src/main/resources/db/migrations/V1__create_baseline.sql
@@ -1,23 +1,70 @@
+-- =================== enums ====================
+CREATE TYPE ticket_priority AS ENUM (
+    'LOW',
+    'MEDIUM',
+    'HIGH'
+    );
+
+CREATE TYPE ticket_status AS ENUM (
+    'BACKLOG',
+    'TODO',
+    'IN_PROGRESS',
+    'ON_HOLD',
+    'ON_REVIEW',
+    'DONE'
+    );
+
+CREATE TYPE sprint_status AS ENUM (
+    'PLANNED',
+    'ACTIVE',
+    'COMPLETED'
+    );
+
+CREATE TYPE estimation_type AS ENUM (
+    'STORY_POINTS',
+    'HOURS'
+    );
+
+CREATE TYPE meeting_type AS ENUM (
+    'PLANNING',
+    'DAILY',
+    'REVIEW',
+    'RETROSPECTIVE',
+    'SCRUM_POKER',
+    'REGULAR'
+    );
+
+CREATE TYPE user_role AS ENUM (
+    'ADMIN',
+    'MEMBER'
+    );
+
+CREATE TYPE project_member_role AS ENUM (
+    'OWNER',
+    'STAKEHOLDER',
+    'MANAGER',
+    'DEVELOPER'
+    );
 -- ==================== ticket ==================
 CREATE TABLE IF NOT EXISTS ticket
 (
     id          uuid PRIMARY KEY,
-    title       text NOT NULL,
+    title       text          NOT NULL,
     description text,
-    priority    text,
-    status      text NOT NULL,
+    priority    ticket_priority,
+    status      ticket_status NOT NULL,
     estimation  int,
     created_at  timestamptz DEFAULT now(),
     updated_at  timestamptz,
     deleted_at  timestamptz,
     sprint_id   uuid,
-    project_id  uuid NOT NULL,
+    project_id  uuid          NOT NULL,
 
     CONSTRAINT sprint_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (sprint_id)
             REFERENCES sprint (id),
     CONSTRAINT project_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (project_id)
             REFERENCES project (id)
 );
 
@@ -25,20 +72,20 @@ CREATE TABLE IF NOT EXISTS ticket
 CREATE TABLE IF NOT EXISTS sprint
 (
     id              uuid PRIMARY KEY,
-    name            text NOT NULL,
+    name            text            NOT NULL,
     business_goal   text,
     dev_plan        text,
-    start_date      DATE NOT NULL,
-    end_dat         DATE NOT NULL,
-    status          text NOT NULL,
-    estimation_type text NOT NULL,
+    start_date      DATE            NOT NULL,
+    end_date        DATE            NOT NULL,
+    status          sprint_status   NOT NULL,
+    estimation_type estimation_type NOT NULL,
     created_at      timestamptz default now(),
     updated_at      timestamptz,
     deleted_at      timestamptz,
-    project_id      uuid NOT NULL,
+    project_id      uuid            NOT NULL,
 
     CONSTRAINT project_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (project_id)
             REFERENCES project (id)
 );
 
@@ -46,23 +93,23 @@ CREATE TABLE IF NOT EXISTS sprint
 CREATE TABLE IF NOT EXISTS meeting
 (
     id          uuid PRIMARY KEY,
-    title       text        NOT NULL,
+    title       text         NOT NULL,
     description text,
-    type        text        NOT NULL,
-    starts_at   timestamptz NOT NULL,
-    ends_at     timestamptz NOT NULL,
+    type        meeting_type NOT NULL,
+    starts_at   timestamptz  NOT NULL,
+    ends_at     timestamptz  NOT NULL,
     created_at  timestamptz DEFAULT now(),
     updated_at  timestamptz,
     deleted_at  timestamptz,
     sprint_id   uuid,
-    project_id  uuid        NOT NULL,
+    project_id  uuid         NOT NULL,
 
     CONSTRAINT sprint_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (sprint_id)
             REFERENCES sprint (id),
 
     CONSTRAINT project_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (project_id)
             REFERENCES project (id)
 );
 
@@ -78,7 +125,7 @@ CREATE TABLE IF NOT EXISTS project
     owner_id    uuid NOT NULL,
 
     CONSTRAINT owner_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (owner_id)
             REFERENCES "user" (id)
 );
 
@@ -90,7 +137,7 @@ CREATE TABLE IF NOT EXISTS "user"
     name          text        NOT NULL,
     email         text UNIQUE NOT NULL,
     password_hash text        NOT NULL,
-    role          text        NOT NULL,
+    role          user_role   NOT NULL,
     created_at    timestamptz DEFAULT now(),
     updated_at    timestamptz,
     deleted_at    timestamptz
@@ -100,19 +147,19 @@ CREATE TABLE IF NOT EXISTS "user"
 CREATE TABLE IF NOT EXISTS project_member
 (
     id         uuid PRIMARY KEY,
-    role       text NOT NULL,
+    role       project_member_role NOT NULL,
     created_at timestamptz DEFAULT now(),
     updated_at timestamptz,
     deleted_at timestamptz,
-    user_id    uuid NOT NULL,
-    project_id uuid NOT NULL,
+    user_id    uuid                NOT NULL,
+    project_id uuid                NOT NULL,
 
     CONSTRAINT user_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (user_id)
             REFERENCES "user" (id),
 
     CONSTRAINT project_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (project_id)
             REFERENCES project (id)
 );
 
@@ -127,11 +174,11 @@ CREATE TABLE IF NOT EXISTS member_ticket
     member_id  uuid NOT NULL,
 
     CONSTRAINT ticket_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (ticket_id)
             REFERENCES ticket (id),
 
     CONSTRAINT member_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (member_id)
             REFERENCES project_member (id)
 );
 
@@ -146,10 +193,10 @@ CREATE TABLE IF NOT EXISTS meeting_member
     member_id  uuid NOT NULL,
 
     CONSTRAINT meeting_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (meeting_id)
             REFERENCES meeting (id),
 
     CONSTRAINT member_id
-        FOREIGN KEY (id)
+        FOREIGN KEY (member_id)
             REFERENCES project_member (id)
 );
